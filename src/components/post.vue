ef8a3daedfe8c778f6e9be31939b58b754eb51ba
Skaper777 <skaper777@gmail.com>
2019-10-20 11:02:02 +0300

    20.10.19

--- src/components/post.vue
+++ src/components/post.vue
@@ -2,7 +2,7 @@
   <div class="post">
     <h2 class="post__ttile">{{ title }}</h2>
     <p class="post__text">{{ text }}</p>
-    <button @click="remove">Удалить</button>
+    <button @click="remove()">Удалить</button>
   </div>     
 </template>
 
@@ -10,18 +10,15 @@
 export default {
   name: 'post', 
 
-  methods: {
-    remove() {
-      this.$emit('remove')
-    }
-  },
-
   props: {
     title: {
       type: String
     },
     text: {
       type: String
+    },
+    remove: {
+      type: Function
     }
   }
 }
@@ -29,8 +26,8 @@ export default {
 
 <style scoped lang="sass">
   .post    
-    max-width: 700px
-    margin: 0 auto
+    width: 100%
+    margin: 0 auto   
     margin-top: 20px
     border: 1px solid grey
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,20 +3,26 @@
     <div class="container">
       <div class="container__left">
 
+        <button class="main-page__create-post">Создать пост!</button>
+
         <div class="post-form">
+          <h2 class="post-form__title">Заголовок</h2>
           <input v-model.lazy="newPost.title" type="text" class="post-form__title">
+          <h2 class="post-form__title">Текст поста</h2>
           <textarea v-model.lazy="newPost.text" class="post-form__text"></textarea>
           <button @click="addPost" class="post-form__btn">Опубликовать</button>
         </div> 
 
         <div class="post-container">
-          <post @remove="deletePost(index)" :title="post.title" :text="post.text" v-for="(post, index) in posts" :key="index"></post>
+          <p>Постов: {{ posts.length }}</p>
+          <post :remove="deletePost" :title="post.title" :text="post.text" v-for="(post, index) in posts" :key="index"></post>
         </div> 
 
       </div>
 
       <div class="container__right">
         <sidebar></sidebar>
+
       </div>
           
     </div>
@@ -66,11 +72,42 @@ export default {
   .main-page
     height: 100%
 
+    &__create-post
+      background: #F3DA0B      
+      padding: 10px
+      font-weight: 700
+      font-size: 18px
+      transition: 0.2s
+
+      &:hover,
+      &:active,
+      &:focus 
+        background: white
+
   .post-form
     display: flex
     flex-direction: column    
-    margin: 0 auto
-    padding: 15px
+    margin: 20px auto
+    border: 1px solid lightgrey
+    border-radius: 3px
+    padding: 10px
+    padding-bottom: 20px
+
+    &__title 
+      text-align: left
+      font-size: 20px
+
+    input,
+    textarea 
+      padding: 10px
+      border-radius: 3px
+      margin-bottom: 10px
+      border: 1px solid grey
+
+    textarea
+      resize: none
+      height: 150px
+         
 
     &__btn 
       font-family: 'Open Sans', Arial, sans-serif
